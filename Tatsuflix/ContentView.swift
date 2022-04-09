b3d813ea8ff912a0b38af8876bddd7f54875fd01
Wellington Tatsunori Asahide <tatsunoripai@gmail.com>
2022-04-09 09:22:55 -0400

    Initial Commit

---
+++ Tatsuflix/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tatsuflix
+//
+//  Created by Wellington Tatsunori Asahide on 09/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tatsuflix/TatsuflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TatsuflixApp.swift
+//  Tatsuflix
+//
+//  Created by Wellington Tatsunori Asahide on 09/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TatsuflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
